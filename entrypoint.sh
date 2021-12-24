0341dee7085ef85368a3e5285405ed89d15f4215
ex7remum <44414649+ex7remum@users.noreply.github.com>
2021-12-24 21:02:40 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/src/cv
-pdflatex -output-directory=/mnt/c/Users/yura- -interaction=nonstopmode main.tex
+pdflatex -output-directory=/usr/src/out -interaction=nonstopmode main.tex
